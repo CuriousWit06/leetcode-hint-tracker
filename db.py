c146e507a59e0928e3f2e45a275ee0aa941fb96f
CuriousWit06 <011419082112a@gmail.com>
2026-08-08 20:42:51 +0530

    add db.py; creates problems, submissions and submission_details tables in postgres

---
+++ db.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE problems(title_slug TEXT PRIMARY KEY, title TEXT NOT NULL, difficulty TEXT NOT NULL, status TEXT, topic_tags JSONB);
+CREATE TABLE submissions(id BIGINT PRIMARY KEY, title_slug TEXT NOT NULL REFERENCES problems(title_slug), status_display TEXT NOT NULL, lang TEXT, submitted_at BIGINT);
+CREATE TABLE submission_details(submission_id BIGINT PRIMARY KEY REFERENCES submissions(id), code TEXT, code_output TEXT, expected_output TEXT, last_testcase TEXT, runtime_error TEXT, compile_error TEXT);
+""")
+
+conn.commit()
+cursor.close()
+conn.close()
