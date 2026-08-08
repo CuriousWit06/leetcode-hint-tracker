c14a8cdc4885212545e9763c7e789aff29221d2b
CuriousWit06 <011419082112a@gmail.com>
2026-08-08 12:11:00 +0530

    add get_submissions_for_problems(questionSlug) to fetch and paginate submissions

--- leetcode_client.py
+++ leetcode_client.py
@@ -65,3 +65,36 @@ def get_solved_problems():
         variables["skip"] += 100
         time.sleep(1)
     return results
+
+def get_submissions_for_problem(questionSlug):
+    operationName = "submissionList"
+    headers, cookies = get_auth()
+    query = """
+    query submissionList($offset: Int!, $limit: Int!, $lastKey: String, $questionSlug: String!, $lang: Int, $status: Int) {\n  questionSubmissionList(\n    offset: $offset\n    limit: $limit\n    lastKey: $lastKey\n    questionSlug: $questionSlug\n    lang: $lang\n    status: $status\n  ) {\n    lastKey\n    hasNext\n    submissions {\n      id\n      title\n      titleSlug\n      status\n      statusDisplay\n      lang\n      langName\n      runtime\n      timestamp\n      url\n      isPending\n      memory\n      hasNotes\n      notes\n      flagType\n      frontendId\n      topicTags {\n        id\n      }\n    }\n  }\n}\n
+    """
+    variables = {
+        "questionSlug": questionSlug,
+        "offset": 0,
+        "limit": 20,
+        "lastKey": None
+    }
+    results = []
+    while True:
+        response = requests.post(
+            "https://leetcode.com/graphql",
+            headers = headers,
+            cookies = cookies,
+            json = {
+                "query": query,
+                "variables": variables,
+                "operationName": operationName
+            }
+        )
+        response = response.json()
+        for s in response["data"]["questionSubmissionList"]["submissions"]:
+            results.append(s)
+        if not(response["data"]["questionSubmissionList"]["hasNext"]):
+            break
+        variables["offset"] += variables["limit"]
+        time.sleep(0.8)
+    return results
