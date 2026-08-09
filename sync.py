01bd35044dfbe0d3e329a2fc505ca60cdc3d363d
CuriousWit06 <011419082112a@gmail.com>
2026-08-09 16:22:43 +0530

    add submission syncing for each problem

--- sync.py
+++ sync.py
@@ -15,6 +15,9 @@ cursor = conn.cursor()
 problems = leetcode_client.get_solved_problems()
 for problem in problems:
     cursor.execute("INSERT INTO problems VALUES(%s, %s, %s, %s, %s) ON CONFLICT(title_slug) DO NOTHING;", (problem['titleSlug'], problem['title'], problem['difficulty'], problem['status'], json.dumps(problem['topicTags'])))
+    submissions = leetcode_client.get_submissions_for_problem(problem['titleSlug'])
+    for submission in submissions:
+        cursor.execute("INSERT INTO submissions VALUES(%s, %s, %s, %s, %s) ON CONFLICT(id) DO NOTHING;", (submission['id'], problem['titleSlug'], submission['statusDisplay'], submission['lang'], submission['timestamp']))
     conn.commit()
 
 cursor.close()
