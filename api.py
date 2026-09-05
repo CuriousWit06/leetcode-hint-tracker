1fe32b291d04699f42b0f4b758077d7ec0331381
CuriousWit06 <011419082112a@gmail.com>
2026-09-05 21:13:24 +0530

    fix cors: remove trailing / from allow_origins

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://leetcode-hint-tracker.vercel.app/"],
+    allow_origins = ["https://leetcode-hint-tracker.vercel.app"],
     allow_methods = ["GET", "POST"],
     allow_headers = ["*"]
 )
